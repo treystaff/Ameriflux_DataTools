d7083dfca0b63fa19fe7be682f4c59eb8b9232f0
Trey Stafford <treystaff@gmail.com>
2014-09-29 22:02:32 -0500

    init

---
+++ create_tables.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+conn = sqlite3.connect('/media/sf_D_Drive/CODE/ameriflux_project/testdb')
+c = conn.cursor()
+
+c.execute('''CREATE TABLE sites
+			(ID INTEGER primary key,
+			site_name str,
+			code_name str)''')
+
+c.execute('''CREATE TABLE L2 
+			(ID INTEGER primary key,
+			site_id REFERENCES sites(id),
+			YEAR int,
+			GAP int,
+			DTIME double,
+			HRMIN int,
+			UST double,
+			TA double,
+			WD double,
+			WS double,
+			NEE double,
+			FC double,
+			SFC double,
+			H double,
+			SH double,
+			LE double,
+			SLE double,
+			FG double, 
+			TS1 double,
+			TSdepth1 double,
+			TS2 double,
+			PREC double,
+			RH double,
+			PRESS double,
+			CO2 double,
+			VDP double,
+			SWC1 double,
+			SWC2 double,
+			Rn double,
+			PAR double,
+			Rg double,
+			Rgdiff double,
+			PARout double,
+			Rgl double,
+			RglOut double,
+			H2O double,
+			RE double,
+			GPP double,
+			CO2top double,
+			CO2height double,
+			APAR double,
+			PARdiff double,
+			APARpct double,
+			ZL double)''')
